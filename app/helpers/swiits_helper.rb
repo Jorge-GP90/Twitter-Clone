3c9efd7d11be33fca599caf8725901e8491d6f84
George Garcia <jorge.90gap@gmail.com>
2021-06-22 15:30:31 +0900

    11th Update: index.html.erb and index.css, Created: form.html.erb form.css file

--- app/helpers/swiits_helper.rb
+++ app/helpers/swiits_helper.rb
@@ -1,2 +1,9 @@
 module SwiitsHelper
+    def choose_new_or_edit
+        if action_name == 'new' || action_name == 'create'
+          confirm_switters_path
+        elsif action_name == 'edit'
+          switter_path
+        end
+    end
 end
