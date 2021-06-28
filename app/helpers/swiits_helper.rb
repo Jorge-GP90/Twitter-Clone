553ea198bbd78aeb67d9f94ad209f117eb986c88
George Garcia <jorge.90gap@gmail.com>
2021-06-28 12:19:39 +0900

    8th [ Solving js issue, clreaning files]

--- app/helpers/swiits_helper.rb
+++ app/helpers/swiits_helper.rb
@@ -1,9 +1,9 @@
 module SwiitsHelper
-    def choose_new_or_edit
-        if action_name == 'new' || action_name == 'create'
-          confirm_swiits_path
-        elsif action_name == 'edit'
-          swiit_path
-        end
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'create'
+      confirm_swiits_path
+    elsif action_name == 'edit'
+      swiit_path
     end
+  end
 end
--- app/models/swiit.rb
+++ app/models/swiit.rb
@@ -1,4 +1,4 @@
 class Swiit < ApplicationRecord
-    validates :title, presence: true
-    validates :content, length: {minimum: 1}, length: {maximum: 140}, presence: { message: " is too long (Max. 140 characters) or is not writen yet." }
+  validates :title, presence: true
+  validates :content, length: {minimum: 1}, length: {maximum: 140}, presence: { message: " is too long (Max. 140 characters) or is not writen yet." }
 end
